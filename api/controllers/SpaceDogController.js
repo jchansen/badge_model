d154b46dd62ad7ded231c1579adce18c06991e29
loukitzanna <loukianova@email.arizona.edu>
2015-04-24 14:44:47 -0700

    made space dog api

---
+++ api/controllers/SpaceDogController.js
@@ -0,0 +1,11 @@
+/**
+ * SpaceDogController
+ *
+ * @description :: Server-side logic for managing spacedogs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
