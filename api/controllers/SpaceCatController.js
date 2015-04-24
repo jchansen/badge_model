c7e8cc4d2f540ef55c6a3d6d26f28a66df14018b
loukitzanna <loukianova@email.arizona.edu>
2015-04-24 15:11:14 -0700

    create cat controller

--- api/controllers/SpaceCatController.js
+++ api/controllers/SpaceCatController.js
@@ -5,7 +5,29 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
-module.exports = {
-	
-};
+var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
 
+module.exports = {
+
+  create: function (req, res) {
+
+    // Create data object (monolithic combination of all parameters)
+    // Omit the blacklisted params (like JSONP callback param, etc.)
+    var data = actionUtil.parseValues(req);
+
+    // Create new instance of model using data from params
+    SpaceCat.create(data).exec(function created(err, newInstance) {
+
+      // Differentiate between waterline-originated validation errors
+      // and serious underlying issues. Respond with badRequest if a
+      // validation error is encountered, w/ validation info.
+      if (err) return res.negotiate(err);
+
+
+      // Send JSONP-friendly response if it's supported
+      // (HTTP 201: Created)
+      res.status(201);
+      res.ok(newInstance.toJSON());
+    });
+  }
+}
